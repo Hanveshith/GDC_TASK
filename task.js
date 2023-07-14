7ae08bba951c73125c9a529799101cba376d4ff7
root <root@DESKTOP-RAA4UCT>
2023-07-14 18:57:32 +0530

    added addtask function

--- task.js
+++ task.js
@@ -1,1 +1,30 @@
-console.log("Hello, World!");
+const fs = require('fs');
+const TaskFile = "task.txt";
+const CompletedFile = "completed.txt";
+
+const AvailableTasks = () =>{
+    const data = fs.readFileSync(TaskFile,"utf8");
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    return lines.map((line) => {
+        const [priority,...task] = line.split(' ');
+        return {
+            priority: parseInt(priority),
+            task: task.join(' '),
+        };
+    });
+} 
+
+const WriteTasks = (tasks) => {
+  const lines = tasks.map((task) => `${task.priority} ${task.task}`);
+  const data = lines.join('\n');
+  fs.writeFileSync(TaskFile,data,"utf-8");
+}
+
+
+const AddTask = (priority,task) => {
+    const tasks = AvailableTasks();
+    tasks.push({ priority, task });
+    WriteTasks(tasks);
+    console.log(`Added task: "${task}" with priority ${priority}`);
+} 
